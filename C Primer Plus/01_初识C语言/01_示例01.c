7bad50fd1d4508313d56a87ad32d14994a89c83d
BladeMasterKing <wangjiansheng_sl@163.com>
2024-01-10 01:18:58 +0800

    first commit

---
+++ C Primer Plus/01_初识C语言/01_示例01.c
@@ -0,0 +1,23 @@
+/**
+ * C语言编程步骤
+ * 1.定义程序的目标
+ * 2.设计程序
+ * 3.编写代码
+ * 4.编译
+ * 5.运行程序
+ * 6.测试和调试程序
+ * 7.维护和修改代码
+ */
+
+/**
+* 示例
+*/
+#include <stdio.h>
+int main(void){
+    int dogs;
+    printf("How many dogs have\n");
+    scanf("%d", &dogs);
+    printf("so you have %d dog(s) \n", dogs);
+
+    return 0;
+}
